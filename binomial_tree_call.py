d2db09613726767df6e3fbb7cbbf5b8ed7b1b851
moosecab00se <tfddr90@gmail.com>
2026-02-17 16:45:37 +0000

    added get_price for node lookup

--- binomial_tree_call.py
+++ binomial_tree_call.py
@@ -1,24 +1,36 @@
 import math
 
 class BinNode:
-    def __init__(self, iniprice, j, n, pup = NotImplemented):
-        self.iniprice = iniprice
+    def __init__(self, price, j, n, up, down, pup):
+        self.price = price #intial
         self.j = j #upcount
-        self.n = n #gen start 0
+        self.n = n #gens, start 0
+        self.k = n - j #downcount
+        self.up = up
+        self.down = down
         self.pup = pup #prob up
     
     def lookup_node(self):
         # n choose j
-        coef = math.factorial(self.n) / (math.factorial(self.j) * math.factorial(self.n - self.j))
-
+        coef = math.factorial(self.n) / (math.factorial(self.j) * math.factorial(self.k))
         pdown = 1 - self.pup
-        other = self.n - self.j
 
-        found_chance = coef * (self.pup ** self.j) * (pdown ** other)
+        found_chance = coef * (self.pup ** self.j) * (pdown ** self.k)
         
         return found_chance
+    
+    def get_price(self):
+        total_ups = self.up ** self.j
+        total_downs = self.down ** (self.k)
+        
+        return self.price * total_downs * total_ups
+        
 
 
-node = BinNode(iniprice=100, j=3, n=5, pup=0.7)
+
+
+node = BinNode(price=100, j=3, n=5, up=1.5, down=0.5, pup=0.7)
 
 print(node.lookup_node())
+
+print(node.get_price())
